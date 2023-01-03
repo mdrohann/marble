7affe43b6f258bc50fa006474c7cccc0f3f558fb
mdrohan <mdsajed210@gmail.com>
2023-01-03 20:03:03 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,50 @@
+// navbar-scroll start
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var scroll = $(this).scrollTop();
+    if (scroll > 180) {
+      $(".navbar").addClass("navbar_bg");
+    } else {
+      $(".navbar").removeClass("navbar_bg");
+    }
+
+    if (scroll > 300) {
+      $(".back_to_top").show(1000);
+    } else {
+      $(".back_to_top").hide(1000);
+    }
+  });
+  $(".back_to_top").on("click", function () {
+    $("html, body").animate({ scrollTop: 0 }, 1000);
+  });
+});
+// navbar-scroll end
+
+// type start
+var typed = new Typed(".hero_head", {
+  strings: ["We are a digital and branding agency based in London."],
+  typeSpeed: 80,
+  loop: true,
+});
+// type end
+
+// wow start
+new WOW().init();
+// wow end
+
+// venobox start
+new VenoBox({
+  selector: ".my-image-links",
+  numeration: true,
+  infinigall: true,
+  share: true,
+  spinner: "rotating-plane",
+});
+new VenoBox({
+  selector: ".my-video-links",
+});
+new VenoBox({
+  selector: ".my-custom-links",
+});
+
+// venobox end
